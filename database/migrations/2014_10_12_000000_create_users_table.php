18fc7136242ea74e40e76af42485bb500686bd7d
Bermar Balibalos <bermar@avail.at>
2019-02-19 15:46:37 +0800

    Add laravel auth scaffolding

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,25 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+            $table->increments('user_id');
+            $table->string('first_name', 100);
+            $table->string('last_name', 100);
+            $table->string('middle_name', 100);
+            $table->string('maiden_name', 100);
+            $table->date('birth_date');
+            $table->string('contact_number', 20);
+            $table->string('street');
+            $table->string('city', 100);
+            $table->string('country', 100);
+            $table->string('postal_code', 20);
+            $table->string('status', 20);
+            $table->string('user_type', 20);
+            $table->string('marital_status', 20);
+            $table->string('profile_picture')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('accepted_terms_at')->nullable();
+            $table->timestamp('accepted_privacy_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
