e2ee35c004d91590b640db7c99746906d9bde617
Bermar Balibalos <bermar@avail.at>
2019-02-19 16:37:48 +0800

    Add profile controller

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware(['auth'])->group(function () {
+
+  Route::get('/profile', 'ProfileController@index')->name('profile');
+
+});
